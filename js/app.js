15133a5ec405a2b802efe5a75fef130b5d13a3c4
Livia Rickli <Livia@Livias-MacBook-Pro.local>
2015-10-20 10:46:18 +0200

    Add work pages

--- js/app.js
+++ js/app.js
@@ -1,11 +1,34 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngResource']);
 
-angular.module('app',[])
-    .controller('mainCtrl',function($window, $scope){
+app.controller('mainCtrl',function($window, $scope){
         $scope.scrollPos = 0;
 
         $window.onscroll = function(){
             $scope.scrollPos = document.body.scrollTop || document.documentElement.scrollTop || 0;
             $scope.$apply(); //or simply $scope.$digest();
         };
+    });
+	
+app.controller('workCtrl',function($window, $scope, $resource){
+		var content = $resource('content/work.json').query(function(){
+			$scope.index = 0;
+			$scope.contentItem = content[$scope.index];
+			$scope.next = function(){
+				if($scope.index < content.length-1){
+					$scope.index = $scope.index+1;
+				} else {
+					$scope.index = 0;
+				}
+				$scope.contentItem = content[$scope.index];
+			}
+			
+			$scope.prev = function(){
+				if($scope.index > 0){
+					$scope.index = $scope.index-1;
+				} else {
+					$scope.index = 0;
+				}
+				$scope.contentItem = content[$scope.index];
+			}				
+		});		
     });
